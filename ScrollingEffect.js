0e194df52cec2cf28edb593be96ff41489738311
Vanessa <vanessapcg@gmail.com>
2016-03-15 14:21:19 -0430

    Changes in header's nav bar

--- ScrollingEffect.js
+++ ScrollingEffect.js
@@ -3,11 +3,22 @@ $(window).scroll(function(){
   var scroll = $(window).scrollTop();
 
   if (scroll > 0 ) {
-    $('nav').addClass('scrolled');
+
+ 
+    $('nav').css("background-color","white");
+    $('.menu__link').css("color", "#4b4b4b");
+    $("#LogoImage").attr("src", "LogoBlue.png");
+
+ 
   }
 
   if (scroll <= 0 ) {
     $('nav').removeClass('scrolled');
+    $('nav').css("background-color", "transparent");
+    $('.menu__link').css("color", "#b5b5b5");
+    $("#LogoImage").attr("src", "Logo.png");
+    $('.menu__item--current').css("color", "white");
+    $('.menu__link::after').css("color", "white");
  }
 
 });
