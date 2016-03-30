11046a5c25da01a140032835cd22a896bdb38b1b
mingoarte <domingoarte2850@gmail.com>
2016-03-30 11:43:44 -0400

    aqui la barra ya pasa por una sola transicion al pasar por soluciones y servicios

--- scripts/ScrollingEffect.js
+++ scripts/ScrollingEffect.js
@@ -4,12 +4,10 @@ $(window).scroll(function(){
 
   if (scroll > 0 ) {
     $('nav').addClass('scrolled');
-    $('.menu__link').css("color", "#4b4b4b");
-    $("#LogoImage").attr("src", "images/LogoBlue.png");
     $('nav').addClass('scrolled');
-    $( "nav" ).css("background-color", "white");
-    $( "nav" ).css("z-index", "10000000");
-    $( "nav" ).css("height", "140");
+    $('.menu__link').css("color", "#b5b5b5");
+    
+
 
 
 
@@ -20,7 +18,6 @@ $(window).scroll(function(){
     $('nav').removeClass('scrolled');
     $('nav').css("background-color", "transparent");
     $('.menu__link').css("color", "#b5b5b5");
-    $("#LogoImage").attr("src", "images/Logo.png");
 
 
 
