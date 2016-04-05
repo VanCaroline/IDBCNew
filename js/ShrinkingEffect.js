6cd52ca72716b78095b188c0643374fcfbbd4734
Vanessa <vanessapcg@gmail.com>
2016-04-05 06:36:34 -0430

    general changes
    general changes

--- js/ShrinkingEffect.js
+++ js/ShrinkingEffect.js
@@ -3,14 +3,19 @@
         window.addEventListener('scroll', function(e){
             var distanceY = window.pageYOffset || document.documentElement.scrollTop,
                 shrinkOn = 250,
-                header = document.querySelector("container");
+                header = document.querySelector("header");
             if (distanceY > shrinkOn) {
                 classie.add(header,"smaller");
                 $( "nav" ).css("background-color", "white");
-                $("#LogoImage").attr("src", "../img/LogoBlue.png");
+                $("#LogoImage").attr("src", "img/LogoBlue.png");
                 $("#LogoImage").css("width","120px");
                 $("#LogoImage").css("height","80px");
                 $("#LogoImage").css("margin-top","3px");
+                $(".menu__link").css("color","#b5b5b5");
+                $(".menu__link").css("z-index","10000000000");
+                $(".menu__shylock").css("top","-30px");
+
+
 
 
 
@@ -21,6 +26,7 @@
                     $("#LogoImage").attr("src", "img/Logo.png");
                     $("#LogoImage").css("width","210px");
                     $("#LogoImage").css("height","120px");
+                    $(".menu__link").css("color","white");
 
 
 
